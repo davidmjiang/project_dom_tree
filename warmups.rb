c7fe4f5e5d6a5c68456392a66d8f6953b6778c49
David Jiang <davidjgrey@gmail>
2016-07-15 14:04:21 -0700

    warmup 2

--- warmups.rb
+++ warmups.rb
@@ -19,21 +19,37 @@ end
 
 parse_tag("<p class='foo bar' id='baz' name='fozzie'>")
 
-Node = Struct.new(:tag, :data)
+Node = Struct.new(:tag, :data, :children)
 
 class HTMLParser
 
   def initialize(html)
-    parser(html)
+    @root = Node.new("html",html, []) 
+    parser(@root)
   end
 
-  def parser(html)
-    Node.new(/<(.*?)>/)
+  def parser(node)
+    first_match = node.data.match(/<(.*?)>/)
+
+    (/<\/(.*?)>/)
+
+    first_tag = first_match[1]
+    rest_of_string = first_match.post_match
+    node.children << Node.new(parse_tag(first_tag), rest_of_string, nil) 
+
+    next_match = root.rest_of_string.match(/<(.*?)\s/)
+    next_tag = next_match[1]
+    rest_of_it = next_tag.post_match
+    next_node = Node.new(parse_tag(next_tag), rest_of_it, root) # p
   end
 
 /<(.*?)>/
 
 # "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
+
+#find the first tag in the string
+#make a new node with tag = parse_tag(that tag) and data = the rest. this is the root.
+#repeat with the rest of the tags inside the root's data, setting their parent to the root
 
 # <div>
 #   div text before
