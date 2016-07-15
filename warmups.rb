1f1e97c8f1727a34608a858752f179526211030a
Mike Lee <asackofwheat@gmail.com>
2016-07-15 13:56:07 -0400

    working on parser

--- warmups.rb
+++ warmups.rb
@@ -1,5 +1,44 @@
-def parse_tag(tag)
+def parse_tag(string)
+  tag_hash = {}
+  tag_hash[:type] = string.match(/<(.*?)\s/)[1]
+  tag_hash[:id] = string.match(/id='(.*?)'/)[1]
+  tag_hash[:name] = string.match(/name='(.*?)'/)[1]
+  tag_hash[:class] = string.match(/class='(.*?)'/)[1].split(" ")
+  binding.pry
+end
+
+# "<p class='foo bar' id='baz' name='fozzie'>"
+
+# tag.type /<.*?\s/
+# => "p"
+# tag.classes /class='(.*?)'/
+# => ["foo", "bar"]
+# tag.id /id='(.*?)'/
+# => "baz"
+# tag.name /name='(.*?)'/
+# => "fozzie"
+
+parse_tag("<p class='foo bar' id='baz' name='fozzie'>")
+
+Node = Struct.new(:tag, :data, :children, :parent)
+
+class HTMLParser
+
+  def initialize(html)
+    parser(html)
+  end
+
+  def parser(html)
+    Node.new(/<(.*?)>/)
+  end
+
+/<(.*?)>/
+
+
+
+
+
+
 
 end
 
-#type /<(...)\s/
