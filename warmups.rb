475d0504d63a8b2ec80055fbb2348e2e76096520
David Jiang <davidjgrey@gmail>
2016-07-15 16:45:46 -0700

    data structure

--- warmups.rb
+++ warmups.rb
@@ -19,30 +19,51 @@ end
 
 parse_tag("<p class='foo bar' id='baz' name='fozzie'>")
 
-Node = Struct.new(:tag, :data, :children)
+#Node = Struct.new(:tag, :data, :children)
+Node = Struct.new(:tag, :children, :parent)
 
 class HTMLParser
 
+attr_reader :root
+
   OPENING = /<([a-z]*?)>/
   CLOSING = /<\/([a-z]*?)>/
-  BOTH = /<(.*?)>/
+  BOTH = /<.*?>/
 
   def initialize(html)
-    @root = Node.new("html",html, []) 
+    @root = Node.new("html", [], nil) 
+    @html = html
     @stack = []
     parser(@root)
-    @html = html
   end
 
   def tags
-    @html.scan(/<.*?>/)
+    @html.scan(BOTH)
   end
 
   def text
-    @html.split(/<.*?>/)
+    @html.split(BOTH)
   end
 
   def parser(node)
+    tags_array = tags
+    text_array = text[1..-1]
+    current_parent = @root
+    current_node = Node.new(tags_array.shift,[], @root)
+    current_parent.children << current_node
+    current_node.children << text_array.shift
+    until tags_array.empty?
+      if tags_array[0].match(/\/#{current_node.tag[1..-2]}/)
+        current_node = current_node.parent
+        tags_array.shift
+        current_node.children << text_array.shift
+      else
+        current_parent = current_node
+        current_node = Node.new(tags_array.shift,[],current_parent)
+        current_parent.children << current_node
+        current_node.children << text_array.shift
+      end
+    end
 
   end
 
@@ -90,7 +111,4 @@ class HTMLParser
 end
 
 h = HTMLParser.new("<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>")
-
-
-puts h.tags
 
